7b37447eb7f545ec39223d9c20a5ae5012b751e0
Huy Lê <92799887+Huyle24@users.noreply.github.com>
2023-06-15 10:13:25 +0700

    Add files via upload

---
+++ CategoryBox.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="card" style="width: 18rem;">
+        <div class="card-img">
+            <img :src="category.imageUrl" class="card-img-top" alt="...">
+        </div>
+        <div class="card-body">
+            <router-link :to="{ name: 'ListProducts', params: { id: category.id } }">
+                <h5 class="card-title">{{ category.categoryName }}</h5>
+            </router-link>
+            <p class="card-text">{{ category.description }}</p>
+            <router-link :to="{ name: 'EditCategory', params: { id: category.id } }" v-show="$route.name == 'CategoryAdmin'">
+                <button href="#" class="btn btn-primary">Edit</button>
+            </router-link>
+
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'CategoryBox',
+    props: ["category"],
+    methods: {},
+}
+</script>
+<style scoped>
+.card .card-img {
+    height: 300px;
+    overflow: hidden;
+
+}
+
+.card .card-img img {
+    transition: all 0.3s;
+}
+
+.card .card-img:hover img {
+    transform: scale(1.2);
+}
+</style>
---
+++ CategoryBoxHome.vue
@@ -0,0 +1,33 @@
+<template>
+    <router-link class="item-category" :to="{ name: 'ListProducts', params: { id: category.id } }">
+        <div class="card-img">
+            <img :src="category.imageUrl" class="card-img-top" alt="...">
+            <h5 class="card-title">{{ category.categoryName }}</h5>
+            <p class="card-text">{{ category.description }}</p>
+        </div>
+    </router-link>
+</template>
+
+<script>
+export default {
+    name: 'CategoryBoxHome',
+    props: ["category"],
+    methods: {},
+}
+</script>
+<style scoped>
+
+.item-category .card-img{
+    align-items: center;
+    height: 250px;
+    overflow: hidden;
+
+}
+.item-category .card-img img {
+    transition: all 0.3s;
+}
+
+.item-category .card-img:hover img {
+    transform: scale(1.2);
+}
+</style>
